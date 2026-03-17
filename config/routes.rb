247a6d4be962c96db42b8dc811c9474925f1dcc1
shuquanit-ui <shuquanit@gmail.com>
2026-03-17 15:48:38 +0800

    fix: simplify glossary admin entry flow

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ end
 
 Discourse::Application.routes.draw do
   get "/admin/plugins/keyword-glossary" =>
+        redirect("/admin/plugins/keyword-glossary/entries")
+
+  get "/admin/plugins/keyword-glossary/settings" =>
         "admin/plugins#show",
       defaults: {
         plugin_id: "keyword-glossary",
