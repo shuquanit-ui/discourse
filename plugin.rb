a374d16ee44f894ecdf568de93584ecb8d4d1c62
shuquanit-ui <shuquanit@gmail.com>
2026-03-17 19:57:09 +0800

    fix: use stable admin plugin root route

--- plugin.rb
+++ plugin.rb
@@ -11,7 +11,7 @@ enabled_site_setting :keyword_glossary_enabled
 register_asset "stylesheets/common/keyword-glossary.scss"
 register_svg_icon "book-open"
 
-add_admin_route "keyword_glossary.title", "keyword-glossary/entries", use_new_show_route: true
+add_admin_route "keyword_glossary.title", "keyword-glossary", use_new_show_route: true
 
 module ::DiscourseKeywordGlossary
   PLUGIN_NAME = "discourse-keyword-glossary"
