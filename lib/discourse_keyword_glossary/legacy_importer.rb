a0106e513ff324142fa357ee8efb823d1c5ce50e
shuquanit-ui <shuquanit@gmail.com>
2026-03-17 11:10:05 +0800

    fix: skip malformed legacy glossary imports

--- lib/discourse_keyword_glossary/legacy_importer.rb
+++ lib/discourse_keyword_glossary/legacy_importer.rb
@@ -12,17 +12,15 @@ module DiscourseKeywordGlossary
         return { imported: 0, skipped: true }
       end
 
-      raw_entries = SiteSetting.keyword_glossary_entries
+      raw_entries = normalize_legacy_entries(SiteSetting.keyword_glossary_entries)
       return { imported: 0, skipped: true } if raw_entries.blank?
 
       imported = 0
 
       GlossaryEntry.transaction do
-        raw_entries.to_s.split("\n").each do |line|
-          next if line.blank?
-
+        raw_entries.each do |line|
           term, description, link_url = line.split("|", 3).map { |value| value.to_s.strip }
-          next if term.blank? || description.blank?
+          next if term.blank? || description.blank? || link_url.to_s.include?("|")
 
           GlossaryEntry.create!(
             term: term,
@@ -43,13 +41,22 @@ module DiscourseKeywordGlossary
     def self.status
       {
         imported_at: PluginStore.get(PLUGIN_NAME, STORE_KEY),
-        has_legacy_entries: SiteSetting.keyword_glossary_entries.present?,
+        has_legacy_entries: normalize_legacy_entries(SiteSetting.keyword_glossary_entries).present?,
       }
     end
 
     def self.import_now!
       result = import_if_needed!
       status.merge(result)
+    end
+
+    def self.normalize_legacy_entries(value)
+      items = value.is_a?(Array) ? value : value.to_s.split("\n")
+      items
+        .map(&:to_s)
+        .map(&:strip)
+        .reject(&:blank?)
+        .select { |line| line.count("|") >= 2 }
     end
   end
 end
